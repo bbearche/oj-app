6e974d85da03b809b305a7ce84f5c00086f90e9b
Brock Bearchell <brock@spacestud.io>
2017-04-04 00:27:59 -0400

    Added reviews to user pages.

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -17,6 +17,27 @@ export class ProfilePage {
   user: any;
 
   /**
+   * The orange types for review score.
+   *
+   * @type {any[]}
+   */
+  orangeTypes: any[] = [];
+
+  /**
+   * The users reviews.
+   *
+   * @type {any[]}
+   */
+  reviews: any[] = [];
+
+  /**
+   * The loading state of the component.
+   *
+   * @type {boolean}
+   */
+  loading: boolean = false;
+
+  /**
    * The constructor for the class.
    *
    * @param  {NavController}  nav
@@ -38,10 +59,15 @@ export class ProfilePage {
     if (this.params.data.id) {
       this.userService.getUser(this.params.data.id).then((res) => {
         this.user = res.data;
+        this.calculateOranges(this.user.total_score);
+        this.reviews = this.user.reviews.data;
       })
     } else {
       this.user = this.auth.user();
+      this.calculateOranges(this.user.total_score);
+      this.reviews = this.user.reviews.data;
     }
+
   }
 
   /**
@@ -50,4 +76,44 @@ export class ProfilePage {
   goToInfo(user: number) {
     this.nav.push(ProfileInfoPage, { user: user });
   }
+
+  /**
+   * Calculates orange types.
+   *
+   * @param  {number} score
+   * @return {any}
+   */
+  calculateOranges(score: number): any {
+    var count;
+
+    for (count = 1; count <= score; count++) {
+      this.orangeTypes.push('full');
+      if ((score - count) < 1 && (score - count) > 0) {
+        this.orangeTypes.push('half');
+      }
+    }
+  }
+
+  /**
+     * Get more reviews.
+     *
+     * @param {any} event
+     */
+  getMoreReviews(infiniteScroll: any): void {
+    if (this.user.reviews.next_page_url) {
+      this.loading = true;
+
+      this.userService.getUser(
+        this.user.id, { page: this.user.reviews.current_page + 1 }
+      ).then((res) => {
+        res.data.reviews.data.forEach(review => this.reviews.push(review));
+        this.user = res.data;
+        this.loading = false;
+        infiniteScroll.complete();
+      })
+    } else {
+      infiniteScroll.complete();
+    }
+  }
+
 }
